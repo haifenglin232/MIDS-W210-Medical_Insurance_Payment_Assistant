715c9f83910c6c9939383f7d11997092296c85a5
haifenglin232 <35426714+haifenglin232@users.noreply.github.com>
2020-06-26 21:19:33 -0400

    Update FAQ.js

--- frontend/src/pages/FAQ.js
+++ frontend/src/pages/FAQ.js
@@ -4,7 +4,7 @@ export const FAQ = () => (
   <div>
     <h2>FAQ</h2>
     <p>
-      Write about HIPPA compliance, success of our model, limitations, GitHub
+      Write about HIPPA ccompliance, success of our model, limitations, GitHub
       repo url etc.
     </p>
   </div>
